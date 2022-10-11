d04b424a343e3b14f7c5ea20d3b8a0b9c20b2773
miamitsahoo@gmail.com <miamitsahoo@gmail.com>
2022-10-11 22:08:01 +0530

    Added Code

--- 35-Grid-Unique-Paths.cpp
+++ 35-Grid-Unique-Paths.cpp
@@ -27,53 +27,12 @@ public:
 
     int tabulation(int m, int n)
     {
-        int dp[m][n];
-        memset(dp, -1, sizeof(dp));
-        dp[0][0] = 1;
-
-        for (int i = 0; i < m; ++i)
-            dp[i][0] = 0;
-
-        for (int j = 0; j < n; ++j)
-            dp[0][j] = 0;
+        vector<vector<int>> dp(m, vector<int>(n, 1));
 
         for (int i = 1; i < m; ++i)
             for (int j = 1; j < n; ++j)
-            {
-                int left = 0, up = 0;
-                if (i > 0)
-                    up = dp[i - 1][j];
-                if (j > 0)
-                    left = dp[i][j - 1];
-                dp[i][j] = up + left;
-            }
-
-        return dp[m - 1][n - 1];
-    }
-
-    int tabulation(int m, int n)
-    {
-        vector<vector<int>> dp(m, vector<int>(n, 1));
-        for (int i = 1; i < m; i++)
-        {
-            for (int j = 1; j < n; j++)
-            {
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-            }
-        }
-        return dp[m - 1][n - 1];
-    }
 
-    int tabs(int m, int n)
-    {
-        vector<vector<int>> dp(m, vector<int>(n, 1));
-        for (int i = 1; i < m; i++)
-        {
-            for (int j = 1; j < n; j++)
-            {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-            }
-        }
         return dp[m - 1][n - 1];
     }
 
